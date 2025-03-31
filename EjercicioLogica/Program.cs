43943e428e3694342d0f7f46392016e748ffdcbd
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-30 21:14:42 -0500

    Agregar proyecto EjercicioLogica y archivo Program.cs
    Se creó el archivo de proyecto `EjercicioLogica.csproj` con la estructura adecuada, especificando el SDK `Microsoft.NET.Sdk`, el tipo de salida `Exe`, el marco de trabajo `net9.0`, y habilitando usings implícitos y tipos de referencia anulables.
    
    Se añadió un nuevo archivo fuente en C# `Program.cs`, que contiene un método `Main` que imprime un mensaje de bienvenida e itera de 0 a 100, imprimiendo "Fizz" para múltiplos de 3, "Buzz" para múltiplos de 5 y "FizzBuzz" para múltiplos de ambos.
    
    Se actualizó el archivo de solución `EjerciciosBasicos_C#.sln` para incluir el nuevo proyecto `EjercicioLogica`, permitiendo que forme parte de la solución junto a los proyectos existentes.
    
    Se modificó la sección global del archivo de solución para incluir configuraciones para el nuevo proyecto, asegurando que se pueda compilar y ejecutar en modos Debug y Release.

---
+++ EjercicioLogica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EjercicioLogica
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("          BIENVENIDO");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Multiplos de 3: \"Fizz\"");
+            Console.WriteLine("Multiplos de 5: \"Buzz\"");
+            Console.WriteLine("Multiplos de 3 y 5: \"FizzBuzz\"");
+            Console.WriteLine("-------------------------------");
+            for (int i = 0; i <= 100; i++) 
+            {
+                if(i % 3 == 0 && i % 5 == 0) // si i es multiplo de 3 y 5
+                {
+                    Console.WriteLine($"\n{i} = \"FizzBuzz\"");
+                }
+                else if (i % 3 == 0) // si i es multiplo de 3
+                {
+                    Console.WriteLine($"\n{i} = \"Fizz\"");
+                }
+                else if (i % 5 == 0) // si i es multiplo de 5
+                {
+                    Console.WriteLine($"\n{i} = \"Buzz\"");
+                }
+                
+            }
+        }
+
+    }
+}
