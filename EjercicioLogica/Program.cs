a29310cfd701e2c58c04817642136693f2b9091e
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-30 22:02:29 -0500

    Cambia tipos de datos de byte a int en FizzBuzz
    Se modificaron los tipos de datos de varias variables y arreglos de `byte` a `int` para acomodar valores más grandes. Específicamente, `countMultiplos3`, `countMultiplos5` y `countMultiplos3y5` se cambiaron a `int`. Los arreglos `FizzBuzz`, `Fizz` y `Buzz` también se cambiaron a `int[]`, con un tamaño de 101. La variable de bucle `i` se cambió a `int` para asegurar la compatibilidad. Se actualizaron las lógicas de llenado de los arreglos para incluir verificaciones de longitud y se utilizaron contadores para almacenar valores. Finalmente, se modificaron las declaraciones de salida para imprimir solo valores diferentes de cero y se añadieron mensajes adicionales para mayor claridad.

--- EjercicioLogica/Program.cs
+++ EjercicioLogica/Program.cs
@@ -6,9 +6,9 @@ namespace EjercicioLogica
     {
         public static void Main(string[] args)
         {
-            byte countMultiplos3 = 0;
-            byte countMultiplos5 = 0;
-            byte countMultiplos3y5 = 0;
+            int countMultiplos3 = 0;
+            int countMultiplos5 = 0;
+            int countMultiplos3y5 = 0;
             Console.WriteLine("-------------------------------");
             Console.WriteLine("          BIENVENIDO");
             Console.WriteLine("-------------------------------");
@@ -36,47 +36,78 @@ namespace EjercicioLogica
                 
             }
 
-            byte[] FizzBuzz = new byte[countMultiplos3y5];
-            byte[] Fizz = new byte[countMultiplos3];
-            byte[] Buzz = new byte[countMultiplos5];
-
+            int[] FizzBuzz = new int[101];
+            int[] Fizz = new int[101];
+            int[] Buzz = new int[101];
+            int countFizzBuzz = 0;
+            int countFizz = 0;
+            int countBuzz = 0;
             Console.WriteLine("-------------------------------");
-            for (byte i = 0; i <= 100; i++)
+            for (int i = 0; i <= 100; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0) // si i es multiplo de 3 y 5
+                if(i < FizzBuzz.Length)
                 {
-                    FizzBuzz[i] = i; // En El Array FizzBuzz se almacena los multiplos de 3 y 5
+
+                    if (i % 3 == 0 && i % 5 == 0) // si i es multiplo de 3 y 5
+                    {
+                        FizzBuzz[countFizzBuzz] = i; // En El Array FizzBuzz se almacena los multiplos de 3 y 5
+                        countFizzBuzz++;   
+
+                    }
                 }
-                else if (i % 3 == 0) // si i es multiplo de 3
+
+                if(i < Fizz.Length)
                 {
-                    Fizz[i] = i; // En el Array Fizz se almacena los multiplos de 3
+                    if (i % 3 == 0) // si i es multiplo de 3
+                    {
+                        Fizz[countFizz] = i; // En el Array Fizz se almacena los multiplos de 3
+                        countFizz++;
+                    }
                 }
-                else if (i % 5 == 0) // si i es multiplo de 5
-                {
-                    Buzz[i] = i; // En el Array Buzz se almacena los multiplos de 5
+
+                if (i < Buzz.Length) {
+
+                    if (i % 5 == 0) // si i es multiplo de 5
+                    {
+                        Buzz[countBuzz] = i; // En el Array Buzz se almacena los multiplos de 5
+                        countBuzz++;
+                    }
                 }
 
             }
 
             Console.WriteLine("Dentro Del Array FizzBuzz Estan Los Elementos:");
+            Console.WriteLine("Multiplos De 3 y 5:");
             for(int i = 0; i < FizzBuzz.Length; i++)
             {
-                Console.WriteLine($"FizzBuzz[{i}] -> {FizzBuzz[i]}");
+                if (FizzBuzz[i] != 0)
+                {
+                    Console.WriteLine($"FizzBuzz[{i}] -> {FizzBuzz[i]}");
+                }
             }
             Console.WriteLine("-------------------------------");
+            Console.WriteLine("Multiplos De 3:");
             Console.WriteLine("Dentro Del Array Fizz Estan Los Elementos:");
             for (int j = 0; j < Fizz.Length; j++)
             {
-                Console.WriteLine($"Fizz[{j}] -> {Fizz[j]}");
+                if (Fizz[j] != 0)
+                {
+                    Console.WriteLine($"Fizz[{j}] -> {Fizz[j]}");
+                }
             }
             Console.WriteLine("-------------------------------");
             Console.WriteLine("Dentro Del Array Buzz Estan Los Elementos:");
+            Console.WriteLine("Multiplos De 5:");
             for (int k = 0; k < Buzz.Length; k++)
             {
-                Console.WriteLine($"Fizz[{k}] -> {Buzz[k]}");
+                if (Buzz[k] != 0)
+                {
+                    Console.WriteLine($"Fizz[{k}] -> {Buzz[k]}");
+                }
             }
             Console.WriteLine("-------------------------------");
 
+            Console.WriteLine("Presione Una Tecla Para Salir");
             Console.ReadKey();
 
         }
