1e5cce78cd323967099363acf87a82d510f52481
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-02 11:42:17 -0500

    Agregar proyecto EjercicioRectangulo y definir su estructura básica

---
+++ EjercicioRectangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EjercicioRectangulo
+{
+    class Program
+    {
+        static float areaR(float Base, float Altura)
+        {
+            float R = 0f;
+            R = (float) Math.Pow(Base, 2) + (float) Math.Pow(Altura, 2);
+            return R;
+        }
+        
+        static void Main(string[] args)
+        {
+            float Base = 0f, Altura = 0f;
+            do
+            {
+
+            } while (Base < 0 || Altura < 0);
+        }
+    }
+}
